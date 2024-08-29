548c29d7beb3c40fadb34b47414c4fac6e5e3a94
lauragomez18 <lalitagomes7@gmail.com>
2024-08-28 20:58:02 -0500

    modificaciones en el index

--- Front/src/app/homepage/body/body.component.ts
+++ Front/src/app/homepage/body/body.component.ts
@@ -1,15 +1,54 @@
-import { Component } from '@angular/core';
+
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-body',
-  standalone: true,
-  imports: [],
   templateUrl: './body.component.html',
-  styleUrl: './body.component.scss'
+  styleUrls: ['./body.component.scss']
 })
-export class BodyComponent {
+export class BodyComponent implements OnInit {
 
+  constructor() { }
+
+  ngOnInit(): void {
+    this.initDatePickers();
+  }
+
+  initDatePickers(): void {
+    
+    const checkIn = document.getElementById('check-in') as HTMLInputElement;
+    const checkOut = document.getElementById('check-out') as HTMLInputElement;
+    const botonReserva = document.getElementById('boton-reserva') as HTMLButtonElement;
+
+    // establecer la fechq minima del el calendario 
+    const hoy = new Date().toISOString().split('T')[0];
+    checkIn.min = hoy;
+    checkOut.min = hoy;
+
+    // validacijon de las fechas 
+    const validarFechas = () => {
+      const fechaEntrada = new Date(checkIn.value);
+      const fechaSalida = new Date(checkOut.value);
+      botonReserva.disabled = !(fechaSalida > fechaEntrada && checkIn.value && checkOut.value);
+    };
+
+    checkIn.addEventListener('change', () => {
+      checkOut.min = checkIn.value;
+      validarFechas();
+    });
+
+    checkOut.addEventListener('change', validarFechas);
+
+    botonReserva.addEventListener('click', () => {
+      if (!botonReserva.disabled) {
+        window.location.href = 'reserva.html'; 
+      }
+    });
+  }
 }
+
+
+
 
 // import { Component } from '@angular/core';
 // import { HttpClient } from '@angular/common/http';
