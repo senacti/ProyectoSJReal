342626e73584ea85902ef073b4f04d76e50f56d4
Pedro20-lab <pedroburgos988@gmail.com>
2025-04-04 08:08:34 -0500

    Inicio de sesión

--- sjreal/database/factories/UserFactory.php
+++ sjreal/database/factories/UserFactory.php
@@ -25,7 +25,10 @@ class UserFactory extends Factory
     {
         return [
             'name' => fake()->name(),
-            'email' => fake()->unique()->safeEmail(),
+            'last_name' => fake()->lastName(),
+            'telephone' => fake()->phoneNumber(),
+            'num_doc' => fake()->randomNumber(),
+            'email' => fake()->email(),
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
--- sjreal/database/migrations/0001_01_01_000000_create_users_table.php
+++ sjreal/database/migrations/0001_01_01_000000_create_users_table.php
@@ -14,7 +14,11 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('email')->unique();
+            $table->string('last_name');
+            $table->string('telephone');
+            $table->string('email');
+            $table->string('num_doc')->unique();
+            $table->boolean('is_admin')->default(false);
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
